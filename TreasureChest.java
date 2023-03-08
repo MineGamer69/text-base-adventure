8725c9d715a00e3227c460818138989c6aa0b9c4
AnzilottiChristopher <anzilottichr@gmail.com>
2023-03-07 19:24:49 -0500

    Added a treasure chest

---
+++ TreasureChest.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class TreasureChest 
+{
+    private Scanner scan = new Scanner(System.in);
+
+    public void TreasureChestFound()
+    {
+        System.out.println("You found a treasure chest! What do you want to do?\n1. Open Chest\n2. Leave Chest");
+        int input = scan.nextInt();
+
+        if(input == 1)
+        {
+            System.out.println("You found a crown!");
+            TextBasedAdventure.start();
+        }
+        else if(input == 2)
+        {
+            System.out.println("You walked away...");
+            TextBasedAdventure.start();
+        }
+    }
+}
