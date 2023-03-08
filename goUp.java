9ebb8a5631f70c637e138560aa23438a09032b8d
Ceffrece <cheffrece@quinnipiac.edu>
2023-03-07 19:39:15 -0500

    adds a died option

--- goUp.java
+++ goUp.java
@@ -10,5 +10,6 @@ public class goUp {
             case 2: System.out.println("You open the doors in front of you and are met with a big room. You walk inside, and instantly get speared with a stone sword and die.");
             case 3: System.out.println("You climb the ladder to your right, and halfway up, the ladder breaks and you fall onto the slide. You slide on the slide until you fall into a pit of lava and die.");
         }
+        youDied.Died();
     }
 }
